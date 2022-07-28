d256b4489ed59384242032bf20e3aeb64e4137d8
Giovanni Rebouças <giovannireboucas@hotmail.com>
2022-07-27 23:52:07 -0200

    Fixed first 2 steps of OAuth, properly getting code and state on callback endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session
 import requests as re
 
 app = Flask(__name__)
@@ -15,17 +15,28 @@ def githubLogin():
     Start Github OAuth2 flow to authenticate a user
     '''
     firstPhaseURL = "https://github.com/login/oauth/authorize"  # First phase URL for OAuth2 authentication
-    headers = {"Accept": "application/json"}
     payload = {
         "client_id": "",
-        "redirect_uri": "http://127.0.0.1/callback",
+        "redirect_uri": "http://127.0.0.1:5000/callback",
         "scope": "user",
         "state": "testestringmudaressamerda"
     }
 
-    firstPhaseResponse = re.get(firstPhaseURL, headers=headers, params=payload)
+    # Hand build the request URL for the initial authentication procedure
+    url = firstPhaseURL + "?" + '&'.join([f"{param}={value}" for param, value in payload.items()])
 
-    return firstPhaseResponse.content
+    return redirect(url)
+
+
+@app.route("/callback", methods=["GET"])
+def getAuthCallback():
+    '''
+    Receive Github OAuth2 session information (phase 2) after successful user login
+    '''
+    state = request.args.get('state')
+    code = request.args.get('code')
+    print(state, code)
+    return state, code
 
 
 if __name__ == "__main__":
